53a7084939cf46fcfd5cf30a1f5d5838d1c751b2
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2021-02-25 21:47:53 -0300

    Second commit NLW0403

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
 import Head from 'next/head'
+
+
+
+import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+
+
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+
+
       <ExperienceBar />
-
-
       <section>
         <div>
           <Profile />
@@ -19,7 +29,7 @@ export default function Home() {
           <Countdown />
         </div>
 
-
+        <ChallengeBox />
 
         <div>
         </div>
